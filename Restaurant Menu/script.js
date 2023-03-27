4fa4bfb0efbc6b566e65885ee3ec1492dc742f83
djgrubac <grubacdjordje@gmail.com>
2023-03-27 17:29:47 +0200

    Added 'All' button

--- Restaurant Menu/script.js
+++ Restaurant Menu/script.js
@@ -1,31 +1,3 @@
-/*const menuBtns = document.querySelectorAll('.menu-btn');
-const foodItems = document.querySelectorAll('.food-item');
-
-menuBtns.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    // Remove active class from all buttons
-    menuBtns.forEach((btn) => {
-      btn.classList.remove('active-btn');
-    });
-
-    // Add active class to the clicked button
-    e.target.classList.add('active-btn');
-
-    // Get the category of the clicked button
-    const category = e.target.id;
-
-    // Filter the food items based on the category
-    foodItems.forEach((item) => {
-      if (item.classList.contains(category)) {
-        item.style.display = 'grid';
-      } else {
-        item.style.display = 'none';
-      }
-    });
-  });
-});
-*/
-
 const allBtn = document.getElementById('all');
 const featuredBtn = document.getElementById('featured');
 const todaySpecialBtn = document.getElementById('today-special');
