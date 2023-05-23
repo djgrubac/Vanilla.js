dccd2a63c547ec9f04fb3369da5d339f7f42b247
djgrubac <grubacdjordje@gmail.com>
2023-05-23 17:18:53 +0200

    finished with the design and started JS

---
+++ Food Recipe/script.js
@@ -0,0 +1,6 @@
+const searchBtn = document.getElementById('.search-btn');
+const mealList = document.getElementById('meal');
+const mealDetails = document.getElementById('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+//23:14 !!!
